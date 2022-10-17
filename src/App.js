74236f4376835ecfc0eec6a793d98047fc8d2756
JoniSalonen <t1sajo03@students.oamk.fi>
2022-10-17 14:26:29 +0300

    origin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import{ useEffect, useState} from 'react';
+import axios from 'axios';
+
+const URL = 'http://localhost:3001/'
+
 
 function App() {
+  const [task,setTask] = useState([])
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response)=> {
+      setTask(response.data)
+    }).catch (error => {
+      alert(error.response.data.error)
+    })
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <di>
+      <h3>My task</h3>
+      <ol>
+        {task.map(task => (
+          <li key = {task.id}>{task.description}</li>
+        ))}
+      </ol>
+
+    </di>
   );
 }
 
